eebc76fa8734d54ea7bc7968aa48464ded25b1d7
Takumin <akahori.t.24kdgn@gmail.com>
2026-02-13 00:44:30 +0900

    CreateClassRoomページのアクションボタンを追加し、キャンセルボタンのラベルを設定

--- backend/app/Filament/Resources/ClassRooms/Pages/CreateClassRoom.php
+++ backend/app/Filament/Resources/ClassRooms/Pages/CreateClassRoom.php
@@ -4,14 +4,32 @@ namespace App\Filament\Resources\ClassRooms\Pages;
 
 use App\Filament\Resources\ClassRooms\ClassRoomResource;
 use Filament\Resources\Pages\CreateRecord;
+use Filament\Actions\Action;
 
 class CreateClassRoom extends CreateRecord
 {
     protected static string $resource = ClassRoomResource::class;
 
-    // タイトルを「授業の登録」に変更
     public function getTitle(): string 
     {
         return '授業の登録';
+    }
+
+    protected function getCreateFormAction(): Action
+    {
+        return parent::getCreateFormAction()
+            ->label('登録する');
+    }
+
+    protected function getCreateAnotherFormAction(): Action
+    {
+        return parent::getCreateAnotherFormAction()
+            ->hidden(); // これでボタンが消えます
+    }
+
+    protected function getCancelFormAction(): Action
+    {
+        return parent::getCancelFormAction()
+            ->label('キャンセル');
     }
 }
