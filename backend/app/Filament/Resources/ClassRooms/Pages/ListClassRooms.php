0499ac790e8e217bb6d2f339916077c03c25b4f5
Takumin <akahori.t.24kdgn@gmail.com>
2026-02-13 00:41:38 +0900

    授業登録ボタンを追加し、授業一覧ページのヘッダーアクションを更新

--- backend/app/Filament/Resources/ClassRooms/Pages/ListClassRooms.php
+++ backend/app/Filament/Resources/ClassRooms/Pages/ListClassRooms.php
@@ -3,11 +3,21 @@
 namespace App\Filament\Resources\ClassRooms\Pages;
 
 use App\Filament\Resources\ClassRooms\ClassRoomResource;
+use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 
 class ListClassRooms extends ListRecords
 {
     protected static string $resource = ClassRoomResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            // これを追加すると「登録」ボタンが出現します
+            Actions\CreateAction::make()
+                ->label('授業を登録する'),
+        ];
+    }
 
     public function getTitle(): string 
     {
